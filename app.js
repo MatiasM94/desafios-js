edd5858edf58c27812cc128d371c3fe9115f8227
Matias Monzon <matiasmonzon_2004@hotmail.com>
2022-09-08 18:40:25 -0300

    Agregue interaccion con el DOM

--- app.js
+++ app.js
@@ -4,45 +4,38 @@ function bienvenida(saludo){
     alert("Bienvenido " + saludo)
 }
 function menu (){
-    opciones = prompt(`${saludo}, ¿Qué desea hacer?: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver inventario. \n 5: Ver clientes. \n 6: Ver precios con iva \n 7: Cerrar app.`);
+    opciones = prompt(`${saludo}, ¿Qué desea hacer?: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver clientes. \n 5: Cerrar app.`);
 }
 function opcionesMenu(){
-    while(opciones != 7){
+    while(opciones != 5){
         switch(opciones){
             case "1":
                 ingresarProductos();
-                opciones = prompt(`${saludo}, Producto agregado, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver inventario. \n 5: Ver clientes. \n 6: Ver precios con iva \n 7: Cerrar app.`);
+                totalProductos()
+                opciones = prompt(`${saludo}, Producto agregado, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver clientes. \n 5: Cerrar app.`);
                 break;
             case "2":
                 buscarProducto();
-                opciones = prompt(`${saludo}, Producto encontrado, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver inventario. \n 5: Ver clientes. \n 6: Ver precios con iva \n 7: Cerrar app.`);
+                opciones = prompt(`${saludo}, Producto encontrado, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver clientes. \n 5: Cerrar app.`);
                 break;
             case "3":
                 ingresarClientes()
-                opciones = prompt(`${saludo}, Cliente agregado, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver inventario. \n 5: Ver clientes. \n 6: Ver precios con iva \n 7: Cerrar app.`);
+                opciones = prompt(`${saludo}, Cliente agregado, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver clientes. \n 5: Cerrar app.`);
                 break;
             case "4":
-                totalProductos()
-                opciones = prompt(`${saludo}, Tus productos se mostraron por consola, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver inventario. \n 5: Ver clientes. \n 6: Ver precios con iva \n 7: Cerrar app.`);
+                totalClientes()
+                opciones = prompt(`${saludo}, Tus clientes se mostraron por consola, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver clientes. \n 5: Cerrar app.`);
                 break;
             case "5":
-                totalClientes()
-                opciones = prompt(`${saludo}, Tus clientes se mostraron por consola, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver inventario. \n 5: Ver clientes. \n 6: Ver precios con iva \n 7: Cerrar app.`);
-                break;
-            case "6":
-                preciosConIva()
-                opciones = prompt(`${saludo}, Los precios se mostraron por consola, elige una opcion para continuar: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver inventario. \n 5: Ver clientes. \n 6: Ver precios con iva \n 7: Cerrar app.`);
-                break;
-            case "7":
                 alert("Gracias por utilizar nuestra app");
                 break;
             default:
-                opciones = prompt("opcion incorrecta, Qué desea hacer?: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver inventario. \n 5: Ver clientes. \n 6: Ver precios con iva \n 7: Cerrar app.");
+                opciones = prompt("opcion incorrecta, Qué desea hacer?: \n 1: Agregar un producto. \n 2: Buscar un producto \n 3: Agregar un cliente. \n 4: Ver clientes. \n 5: Cerrar app.");
         }
     }
 }
 function ingresarProductos(){
-    productos.push(new Producto(prompt("ingrese el ID del producto"), prompt("ingrese el nombre del producto"), parseInt(prompt("ingrese la cantidad en stock")), Number(prompt("ingrese el precio del producto"))))
+    productos.push(new Producto(prompt("ingrese el numero de orden de lista del producto(ej: 01, 02, 03, etc.):"), prompt("ingrese el ID del producto(id del proveedor)"), prompt("ingrese el nombre del producto"), prompt("ingrese el nombre de la marca del producto"), parseInt(prompt("ingrese la cantidad en stock")), Number(prompt("ingrese el precio del producto"))))
 }
 function buscarProducto(){
     let buscar = prompt("ingrese el nombre del producto que desea buscar");
@@ -58,23 +51,19 @@ function ingresarClientes(){
     clientes.push(new Cliente(prompt("Ingrese el numero de cliente"), prompt("ingrese el nombre del cliente")));
 }
 function totalProductos(){
-    productos.forEach( (producto) => {
-        console.log("Producto:", producto.nombre, "Cantidad", producto.cantidad)
-    })
+    const productoAgregado = document.createElement("div")
+    for (const producto of productos) {
+        productoAgregado.innerHTML = `<div><p>${producto.numero}</p></div><div><p>${producto.id}</p></div><div><p>${producto.nombre}</p></div><div><p>${producto.marca}</p></div><div><p>${producto.cantidad}</p></div><div><p>${producto.precio}</p></div><div>${producto.precio*1.21}</div><div><button class="eliminar"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+        </svg></i></button></div>`
+        productoAgregado.className = "indice-lista__productos"
+        document.querySelector("#lista-productos").appendChild(productoAgregado);
+    }
 }
 function totalClientes(){
     clientes.forEach( (cliente) => {
         console.log("Cliente", cliente.nombre)
     })
-}
-function preciosConIva(){
-    const ivaIncluido = productos.map((iva) => {
-        return {
-        nombre: iva.nombre,
-        precio: iva.precio * 1.21
-        }
-    })
-    console.table(ivaIncluido)
 }
 
 // Aca empieza a ejecutarse la app
@@ -83,12 +72,14 @@ bienvenida(saludo);
 let opciones;
 const productos = [];
 class Producto {
-    constructor(id, nombre, cantidad, precio){
-    this.id = id;
-    this.nombre = nombre;
-    this.cantidad = cantidad;
-    this.precio = precio;
-    }
+    constructor(numero, id, nombre, marca, cantidad, precio){
+        this.numero = numero;
+        this.id = id;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }    
 }
 const clientes = [];
 class Cliente {
@@ -99,6 +90,8 @@ class Cliente {
 }
 menu();
 opcionesMenu();
+
+
 
 
 
